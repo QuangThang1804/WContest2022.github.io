36f0bffe2b49b722b9ec72b252e519e013b2484b
QuangThang1804 <thangpham1804@gmail.com>
2022-09-17 02:33:32 +0700

    update animation

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-var end = new Date(2023, 04, 18).getTime();
+// var end = new Date(2023, 04, 18).getTime();
 
-    var _second = 1000;
-    var _minute = _second * 60;
-    var _hour = _minute * 60;
-    var _day = _hour * 24;
-    var timer;
+//     var _second = 1000;
+//     var _minute = _second * 60;
+//     var _hour = _minute * 60;
+//     var _day = _hour * 24;
+//     var timer;
 
-    function showRemaining() {
-        var now = new Date().getTime();
-        console.log(end);
-        var distance = end - now;
-        if (distance < 0) {
+//     function showRemaining() {
+//         var now = new Date().getTime();
+//         console.log(end);
+//         var distance = end - now;
+//         if (distance < 0) {
 
-            clearInterval(timer);
-            document.getElementById('flipdown').innerHTML = 'EXPIRED!';
-            console.log("days " + days);
-            return;
-        }
+//             clearInterval(timer);
+//             document.getElementById('flipdown').innerHTML = 'EXPIRED!';
+//             console.log("days " + days);
+//             return;
+//         }
 
         
 
-        var days = Math.floor(distance / _day);
-        var hours = Math.floor((distance % _day) / _hour);
-        var minutes = Math.floor((distance % _hour) / _minute);
-        var seconds = Math.floor((distance % _minute) / _second);
-        console.log(days)
+//         var days = Math.floor(distance / _day);
+//         var hours = Math.floor((distance % _day) / _hour);
+//         var minutes = Math.floor((distance % _hour) / _minute);
+//         var seconds = Math.floor((distance % _minute) / _second);
+//         console.log(days)
 
-        document.getElementById('flipdown').innerHTML = days + 'days ';
-        document.getElementById('flipdown').innerHTML += hours + 'hrs ';
-        document.getElementById('flipdown').innerHTML += minutes + 'mins ';
-        document.getElementById('flipdown').innerHTML += seconds + 'secs';
-    }
+//         document.getElementById('flipdown').innerHTML = days + 'days ';
+//         document.getElementById('flipdown').innerHTML += hours + 'hrs ';
+//         document.getElementById('flipdown').innerHTML += minutes + 'mins ';
+//         document.getElementById('flipdown').innerHTML += seconds + 'secs';
+//     }
 
-    timer = setInterval(showRemaining, 1000);
-    console.log(timer)
+//     timer = setInterval(showRemaining, 1000);
+//     console.log(timer)
+
